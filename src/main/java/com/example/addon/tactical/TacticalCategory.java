0afb0b504c1fd358f362c0c47604603b2f3b19e4
你的 GitHub 用户名 < GitHub >
2026-08-25 03:50:53 +0800

    style: 统一分类名称格式为彩色样式
    修改:
    - 分类名称改为: clyiyiaddon dl绕过
    - 与其他分类保持一致的格式风格
    - 使用紫色标注'绕过'关键词

--- src/main/java/com/example/addon/tactical/TacticalCategory.java
+++ src/main/java/com/example/addon/tactical/TacticalCategory.java
@@ -4,7 +4,7 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import net.minecraft.world.item.Items;
 
 /**
- * yiyiaddon 反作弊绕过模块分类
+ * yiyiaddon 绕过模块分类
  * 
  * 专注于服务器反作弊绕过、网络拦截、协议欺骗
  * 
@@ -12,9 +12,6 @@ import net.minecraft.world.item.Items;
  */
 public class TacticalCategory {
     
-    public static final Category TACTICAL = new Category(
-        "yiyiaddon 绕过",
-        () -> Items.ENDER_EYE.getDefaultInstance()
-    );
+    public static final Category TACTICAL = new Category("§c§lyiyiaddon §d§l绕过", () -> Items.ENDER_EYE.getDefaultInstance());
     
 }
